fb656d1e9b818649b2dd428f4c3294a0a96fabd0
zavanton123 <zavanton123@gmail.com>
2021-01-30 14:42:20 +0300

    profile added

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+from .models import ProfileType
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display_links = ('last_name',)
+    ordering = ['id']
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'gender',
+    )
+
+
+@admin.register(ProfileType)
+class ProfileTypeAdmin(admin.ModelAdmin):
+    list_display_links = ('title',)
+    ordering = ['id']
+    list_display = (
+        'id',
+        'title',
+        'description'
+    )
--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    class Gender(models.TextChoices):
+        MALE = 'M',
+        FEMALE = 'F',
+
+    auth_user = models.OneToOneField(
+        to='authentication.AuthUser',
+        on_delete=models.CASCADE,
+        related_name='profiles',
+    )
+    first_name = models.CharField(
+        max_length=100,
+    )
+    last_name = models.CharField(
+        max_length=100,
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=Gender.choices,
+        default=Gender.MALE,
+    )
+    type = models.ForeignKey(
+        to='ProfileType',
+        on_delete=models.CASCADE,
+        related_name='profiles',
+    )
+
+    def __str__(self):
+        return self.last_name
+
+
+class ProfileType(models.Model):
+    title = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField(
+        max_length=1000,
+    )
+
+    def __str__(self):
+        return self.title
